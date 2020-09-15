862b4803afe237971d4f93e2a9c3f918d43d9818
NanoCAD Projects <nanoproj@nanocad-server-testbed.ee.ucla.edu>
2020-09-15 14:51:37 -0700

    change

--- configs/learning_gem5/simple.py
+++ configs/learning_gem5/simple.py
@@ -15,8 +15,6 @@ parser.add_option('--l2_size')
 
 system = System()
 
-system.comm_monitor = CommMonitor()
-system.comm_monitor.trace = MemTraceProbe(trace_file="my_trace.trc")
 
 # clock domain
 system.clk_domain = SrcClockDomain()
@@ -24,15 +22,16 @@ system.clk_domain.clock = '10GHz'
 system.clk_domain.voltage_domain = VoltageDomain(voltage = "5V")
 
 # memory setup
-system.mem_mode = 'timing'
-system.mem_ranges = [AddrRange('512MB')]
+system.mem_mode = 'atomic'
+system.mem_ranges = [AddrRange('512MB'), AddrRange('512MB', '1024MB')]
+
 
 # Memory Bus
 system.membus = SystemXBar()
 system.l2bus = L2XBar()
 
 # CPU
-system.cpu = TimingSimpleCPU()
+system.cpu = AtomicSimpleCPU()
 system.cpu.icache = L1ICache(options)
 system.cpu.dcache = L1DCache(options)
 
@@ -41,8 +40,7 @@ system.cpu.dcache_port = system.cpu.dcache.cpu_side
 
 system.cpu.icache.mem_side = system.l2bus.slave
 
-system.cpu.dcache.mem_side = system.comm_monitor.slave
-system.l2bus.slave = system.comm_monitor.master
+system.cpu.dcache.mem_side = system.l2bus.slave
 
 system.l2cache = L2Cache(options)
 system.l2cache.cpu_side = system.l2bus.master
@@ -60,9 +58,14 @@ system.mem_ctrl = DDR3_1600_8x8()
 system.mem_ctrl.range = system.mem_ranges[0]
 system.mem_ctrl.port = system.membus.master
 
+# SRAM Memory Controller
+system.scratchpad = DDR3_1600_8x8()
+system.scratchpad.range = system.mem_ranges[1]
+system.scratchpad.port = system.membus.master
+
 # Process
 process = Process()
-process.cmd = ['/home/nanoproj/michael/gem5/configs/learning_gem5/sieve_of_eratosthenes', '100']
+process.cmd = ['/home/nanoproj/michael/gem5/configs/learning_gem5/sieve_of_eratosthenes', '5']
 system.cpu.workload = process 
 system.cpu.createThreads()
 
---
+++ configs/sys-emu/nocache.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+SPECROOTDIR=~/michael/spec2017
+GEM5OPT=~/michael/latest-gem5/gem5/build/X86/gem5.opt
+SPEC17CFG=~/michael/gem5/configs/sys-emu/config-files/spec17_config.py
+SIMOPTIONS='--fast-forward=10000000 --maxinsts=300000000 --caches'
+ 
+######################################################################
+
+BENCHMARK="lbm_r"
+OUTDIR=~/michael/gem5/configs/sys-emu/nocache
+
+
+# Set up enviorment
+cd ${SPECROOTDIR}
+source shrc
+
+# Go to executable of that benchmark
+go $BENCHMARK > /dev/null
+ACTUALBENCHNAME=`basename "$PWD"`
+echo "Found Benchmark: $ACTUALBENCHNAME" 
+echo
+
+# Attemt to enter into the run directory
+{
+    cd ./run/run*
+} ||
+{
+    echo "Error: Benchmark $ACTUALBENCHNAME has not been built yet"
+    exit
+}
+
+# Run gem5
+${GEM5OPT} --outdir=${OUTDIR} ${SPEC17CFG} --benchmark ${ACTUALBENCHNAME} ${SIMOPTIONS}
+
